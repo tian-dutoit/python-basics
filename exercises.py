4dc1ac365226ae8cae85ba742af1a99797d0c781
tian-dutoit <tiandutoit.nz@gmail.com>
2018-05-19 15:52:08 +1200

    exercise 33

--- exercises.py
+++ exercises.py
@@ -1,9 +1,13 @@
 def length_function(word):
-    return len(word)  
+    if(type(word) == int):
+        return "Sorry integers don't have length"
+    else:  
+        return len(word)  
 
 def temp_converter(celsius):
     return celsius * 9 / 5 + 32
 
   
 print(length_function("hello how long is this?"))
+print(length_function(5))
 print(temp_converter(10))
