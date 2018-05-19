9b03826e91fd2f24259ab7f102c969c169561243
tian-dutoit <tiandutoit.nz@gmail.com>
2018-05-19 15:10:33 +1200

    exercise 32

--- exercises.py
+++ exercises.py
@@ -1,4 +1,9 @@
 def length_function(word):
     return len(word)  
-    
+
+def temp_converter(celsius):
+    return celsius * 9 / 5 + 32
+
+  
 print(length_function("hello how long is this?"))
+print(temp_converter(10))
